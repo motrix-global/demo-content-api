c46bbb1aa65ed8ba6cd7f1aab3f25f5ffd3d5698
Mateus Garcia <mateus.garcia@motrix.global>
2022-08-26 23:53:14 -0300

    feat: revisions

--- src/content.ts
+++ src/content.ts
@@ -1,12 +1,18 @@
 import { PrismaClient } from "@prisma/client"
-import { randomUUID } from "crypto"
 import { Router } from "express"
 
 const router = Router()
 const prisma = new PrismaClient()
 
 router.get("/", async (req, res) => {
-  const content = await prisma.content.findMany()
+  const content = await prisma.content.findMany({
+    include: {
+      revisions: true,
+    },
+    where: {
+      deletedAt: null,
+    },
+  })
   res.json(content)
 })
 
@@ -15,35 +21,68 @@ router.get(`/:id`, async (req, res) => {
   const content = await prisma.content.findFirst({
     where: { id: Number(id) },
   })
+
   res.json(content)
 })
 
 router.post(`/`, async (req, res) => {
-  const { title, body } = req.body
+  const { title } = req.body
   const result = await prisma.content.create({
     data: {
       title,
-      contentId: randomUUID(),
-      body,
-      published: false,
     },
   })
+
   res.json(result)
 })
 
-router.put("/publish/:id", async (req, res) => {
+router.put("/:id", async (req, res) => {
   const { id } = req.params
-  const content = await prisma.content.update({
-    where: { id: Number(id) },
-    data: { published: true },
-  })
-  res.json(content)
+  const { body } = req.body
+
+  if (!id) res.status(400).json({ error: true, message: "id is required" })
+  if (!body)
+    res.status(400).json({
+      error: true,
+      message: "a 'body' property is required in request body",
+    })
+
+  try {
+    const content = await prisma.content.update({
+      where: { id: Number(id) },
+      data: {
+        revisions: {
+          create: {
+            body: body,
+          },
+        },
+      },
+    })
+
+    res.json(content)
+  } catch (error) {
+    res.status(500).json({ error: true, message: error })
+  }
+
+  console.log(req.body)
 })
+
+// router.put("/publish/:id", async (req, res) => {
+//   const { id } = req.params
+//   const content = await prisma.content.update({
+//     where: { id: Number(id) },
+//     data: { published: true },
+//   })
+//   res.json(content)
+// })
 
 router.delete(`/:id`, async (req, res) => {
   const { id } = req.params
-  const content = await prisma.content.delete({
+  const content = await prisma.content.update({
     where: { id: Number(id) },
+    data: {
+      deletedAt: new Date(),
+    },
   })
   res.json(content)
 })
