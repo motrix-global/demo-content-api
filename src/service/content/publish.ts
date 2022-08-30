2852a5d64d5545c27cefb9f1f0053065ff7e89ec
Mateus Garcia <mateus.garcia@motrix.global>
2022-08-30 03:26:11 -0300

    feat: unpublish

--- src/service/content/publish.ts
+++ src/service/content/publish.ts
@@ -2,11 +2,12 @@ import prisma from "../prisma"
 
 const publishContent = async (req: any) => {
   const { id } = req.params
+  const { publish } = req.body || true
   if (!id) throw new Error("id is required")
 
   const content = await prisma.content.update({
     where: { id: Number(id) },
-    data: { published: true },
+    data: { published: publish },
   })
 
   return content
